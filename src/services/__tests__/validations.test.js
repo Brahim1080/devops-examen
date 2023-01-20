fa52fbdddf9afe08224f7725d536d2e8ed8e0f1b
Brahim <brahim.abid@student.vinci.be>
2023-01-20 10:26:41 +0100

    test(services): test should return false as the label is undefined

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,4 +1,5 @@
 const { isEmpty } = require("../validations");
+const {isValidGamerTag} = require("../validations");
 
 describe("validations tests suites - isEmpty", () => {
     test("should return true as the label is undefined", () => {
@@ -16,5 +17,15 @@ describe("validations tests suites - isEmpty", () => {
         expect(result).toBe(false);
     });
 });
-
 // TODO: Create tests suite for validation function
+
+describe("GamerTag tests suites - isValidGamerTag", () => {
+    test("should return false as the label is undefined", () => {
+        const result = isValidGamerTag();
+        expect(result).toEqual(false);
+    });
+
+
+    
+});
+
--- src/services/validations.js
+++ src/services/validations.js
@@ -1,5 +1,8 @@
 // TODO: Add here the function to validate
 
 const isEmpty = (label) => !label || label.length === 0;
-
+function isValidGamerTag(){
+    return false;
+}
 exports.isEmpty = isEmpty;
+exports.isValidGamerTag = isValidGamerTag;
